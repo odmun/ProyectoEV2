c3669eabfc078b01f71ddc88a9297f61f9f6959f
Alessandro Di Vito Rodríguez <alessandrodivitorodriguez@gmail.com>
2023-01-13 03:01:48 +0100

    Agregados, cambios y modificiones a la interfaz

--- app/src/main/java/com/example/proyectoev2/Login.java
+++ app/src/main/java/com/example/proyectoev2/Login.java
@@ -3,13 +3,23 @@ package com.example.proyectoev2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Arrays;
+
+public class Login extends AppCompatActivity {
+
+    private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+
+        spinner = findViewById(R.id.idiomas);
+
     }
+
 
 }
