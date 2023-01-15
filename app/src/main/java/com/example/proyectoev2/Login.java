8bc949adbabc6c29d46c33317b4b9aa8a7a7c69d
Alejandro.G.H <alejandrogonzahernandez@gmail.com>
2023-01-15 02:00:50 +0100

    Auto stash before merge of "main" and "origin/main"

--- app/src/main/java/com/example/proyectoev2/Login.java
+++ app/src/main/java/com/example/proyectoev2/Login.java
@@ -9,18 +9,48 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
+import java.util.Arrays;
+
 public class Login extends AppCompatActivity {
 
-    private EditText mail, contrasena;
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+    Button logingoogle;
+
+    private EditText mail, contraseña;
     private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        logingoogle = findViewById(R.id.logingoogle);
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc = GoogleSignIn.getClient(this,gso);
+
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+        if (acct!=null){
+            navigateToSecondActivity();
+        }
+
+        logingoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn();
+            }
+        });
 
         mail=findViewById(R.id.mail);
         contrasena =findViewById(R.id.contraseña);
@@ -34,6 +64,35 @@ public class Login extends AppCompatActivity {
             startActivity(intent);
         });
     }
+
+    void signIn() {
+        Intent singInIntent = gsc.getSignInIntent();
+        startActivityForResult(singInIntent,1000);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        if (requestCode==1000){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                task.getResult(ApiException.class);
+                navigateToSecondActivity();
+            }catch (ApiException e) {
+                Toast.makeText(getApplicationContext(),"F",Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    void navigateToSecondActivity(){
+        finish();
+        Intent intent = new Intent(Login.this,MainUser.class);
+        startActivity(intent);
+    }
+
+
+
 
     public void consultaUsuarioPorMailYPassword(View v) {
         RegistroUsuarioSQLite admin = new RegistroUsuarioSQLite(this,
--- app/src/main/java/com/example/proyectoev2/MainUser.java
+++ app/src/main/java/com/example/proyectoev2/MainUser.java
@@ -1,14 +1,60 @@
 package com.example.proyectoev2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MainUser extends AppCompatActivity {
 
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+    Button cerrarsesion;
+    TextView nombre, correo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_user);
+
+        cerrarsesion = findViewById(R.id.cerrarsesion);
+
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc = GoogleSignIn.getClient(this,gso);
+
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+        if (acct!=null){
+            String personaName = acct.getDisplayName();
+            String personaEmail = acct.getEmail();
+            nombre.setText(personaName);
+            correo.setText(personaEmail);
+        }
+
+        cerrarsesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                singOut();
+            }
+        });
+    }
+
+    private void singOut() {
+        gsc.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                finish();
+                startActivity(new Intent(MainUser.this,Login.class));
+            }
+        });
     }
 }
